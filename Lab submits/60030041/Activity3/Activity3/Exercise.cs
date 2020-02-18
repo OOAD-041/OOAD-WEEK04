970b607205b7e109586d95fe32c6f23efe74fc37
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-02-18 22:47:14 +0700

    Update Exercise.cs

--- Lab submits/60030041/Activity3/Activity3/Exercise.cs
+++ Lab submits/60030041/Activity3/Activity3/Exercise.cs
@@ -1,13 +1,23 @@
 ﻿class Exercise
 {
-    ...
-    // ไม่ต้องแก้ไขเมธอด ShowEmployee
-    ...
+    void ShowEmployee(int employeeNumber, string fullName, string gender, double hourlySalary)
+    {
+        System.Console.WriteLine("Employee Record");
+        System.Console.WriteLine("-----------------------------");
+        System.Console.Write("Employee #:    ");
+        System.Console.WriteLine(employeeNumber); // นำพารามิเตอร์มาแสดงที่หน้าจอ
+        System.Console.Write("Full Name:     ");
+        System.Console.WriteLine(fullName); // นำพารามิเตอร์มาแสดงที่หน้าจอ
+        System.Console.Write("Gender:        ");
+        System.Console.WriteLine(gender);   // นำพารามิเตอร์มาแสดงที่หน้าจอ
+        System.Console.Write("Hourly Salary: ");
+        System.Console.WriteLine(hourlySalary); // นำพารามิเตอร์มาแสดงที่หน้าจอ
+    }
 
-    static int Main()
+static int Main()
     {
         Exercise exo = new Exercise();
-
+        
         // เรียกใช้เมธอดโดยระบุชื่อพารามิเตอร์ทั้ง 4 ตัวตามลำดับโดยระบุชื่อพารามิเตอร์
         exo.ShowEmployee(fullName: "Somsri Jaiorn", gender: "F", employeeNumber: 84654);
 
