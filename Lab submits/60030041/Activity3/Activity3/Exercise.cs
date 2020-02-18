752c47e4c1b20852e5d52116d24bef6e881026b9
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-02-18 22:41:27 +0700

    Update Exercise.cs

--- Lab submits/60030041/Activity3/Activity3/Exercise.cs
+++ Lab submits/60030041/Activity3/Activity3/Exercise.cs
@@ -16,9 +16,13 @@
 
     static int Main()
     {
-        Exercise exo = new Exercise();
+       Exercise exo = new Exercise();
         // เรียกใช้งานโดยส่งพารามิเตอร์ทั้ง 4 ตัวไปให้เมธอด
         exo.ShowEmployee(572948, "Sompong Jaidee", "M", 450.00D);
+        // ละพารามิเตอร์ 1 ตัว จากด้านขวามือสุด
+        exo.ShowEmployee(572948, "Somchai Jaided", "M");
+        // ละพารามิเตอร์ 2 ตัว จากด้านขวามือสุด
+        exo.ShowEmployee(572948, "Sommai Jaisoo");
 
         System.Console.ReadKey();
         return 0;
